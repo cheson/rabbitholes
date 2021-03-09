742137793a09f2483961a3a3fbc77921329d2ff5
cheson <json@cs.stanford.edu>
2021-03-09 11:00:46 -0800

    server prettier setup

--- server/app.js
+++ server/app.js
@@ -1,42 +1,37 @@
-const express = require('express');
-const MongoClient = require('mongodb')
-const usersRoutes = require('./routes/users.js')
+const express = require("express");
+const MongoClient = require("mongodb");
+const usersRoutes = require("./routes/users.js");
 
-const dotenv = require('dotenv')
-dotenv.config({path: './dev_secrets/.env'})
+const dotenv = require("dotenv");
+dotenv.config({ path: "./dev_secrets/.env" });
 
 const app = express();
 
 const mongo_uri = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PW}@claustrophobiccluster.7kape.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 MongoClient.connect(mongo_uri, { useNewUrlParser: true })
-.then(client => {
-  app.locals.db = client.db('flow-website');
-}).catch(error => console.error(error));
+  .then((client) => {
+    app.locals.db = client.db("flow-website");
+  })
+  .catch((error) => console.error(error));
 
-app.use(express.static('../client/build'));
+app.use(express.static("../client/build"));
 app.use(express.json());
 
-app.use('/1/users', usersRoutes)
+app.use("/1/users", usersRoutes);
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
+app.get("/api/getList", (req, res) => {
   var list = ["item1", "item25", "item3"];
   res.json(list);
-  console.log('Sent list of items');
+  console.log("Sent list of items");
 });
 
-app.get('/1/flows/:flowId', (req,res) => {
+app.get("/1/flows/:flowId", (req, res) => {
   console.log(`${req.params.flowId}`);
 });
 
-app.get('*', function (req, res) {
-  res.sendFile('../client/build/index.html');
-  });
+app.get("*", function (req, res) {
+  res.sendFile("../client/build/index.html");
+});
 
 app.listen(9000);
-
-
-
-
-
-
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,42 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var admin = require("firebase-admin");
 
-const dotenv = require('dotenv')
-dotenv.config({path: './dev_secrets/.env'})
-
-var serviceAccount = require("../dev_secrets/flow-website-2f43f-firebase-adminsdk-tx4es-32e589224b.json");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./dev_secrets/.env" });
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.applicationDefault(),
 });
 
 // change to post later on and handle incoming parameters
-router.post('/register', (req, res) => {
-    admin
-        .auth()
-        .verifyIdToken(req.body.token)
-        .then((decodedToken) => {
-            const query = { firebase_id: decodedToken.uid };
-            const update = { $set: { firebase_id: decodedToken.uid, name: decodedToken.name, email: decodedToken.email }};
-            const options = { upsert: true };
-            usersCollection = req.app.locals.db.collection('users')
-            usersCollection.updateOne(query, update, options);
-            res.sendStatus(200);
-        })
-        .catch((error) => {
-            console.log(error)
-            res.sendStatus(400);
-        });
-    console.log("registering new user")
+router.post("/register", (req, res) => {
+  admin
+    .auth()
+    .verifyIdToken(req.body.token)
+    .then((decodedToken) => {
+      const query = { firebase_id: decodedToken.uid };
+      const update = {
+        $set: {
+          firebase_id: decodedToken.uid,
+          name: decodedToken.name,
+          email: decodedToken.email,
+        },
+      };
+      const options = { upsert: true };
+      usersCollection = req.app.locals.db.collection("users");
+      usersCollection.updateOne(query, update, options);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(400);
+    });
+  console.log("registering new user");
 });
 
-router.get('/', (req, res) => {
-    usersCollection = req.app.locals.db.collection('users')
-    usersCollection.find({}).toArray().then(response => {
-        console.log(response);
-        res.status(200).json(response);
-      }).catch(error => console.error(error));
+router.get("/", (req, res) => {
+  usersCollection = req.app.locals.db.collection("users");
+  usersCollection
+    .find({})
+    .toArray()
+    .then((response) => {
+      console.log(response);
+      res.status(200).json(response);
+    })
+    .catch((error) => console.error(error));
 });
-    
+
 module.exports = router;
