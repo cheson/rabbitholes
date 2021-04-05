4afbbc9f3fcdd708745bff51a209ec4301b911b7
cheson <json@cs.stanford.edu>
2021-04-05 16:06:15 -0700

    cleaned up dropzone to preview uploaded image + prototyped css grid in create flow component

--- client/src/components/CreateFlow/index.js
+++ client/src/components/CreateFlow/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CreateFlowBlock from "../CreateFlowBlock";
+import styles from "./CreateFlow.module.css";
 
 export default function CreateFlow() {
   const [blocks, setBlocks] = useState(["blue", "green", "black"]);
@@ -10,27 +11,35 @@ export default function CreateFlow() {
 
   return (
     <div>
-      <CreateFlowBlock />
-
       <h2>Create Flow</h2>
-      {blocks.map((block) => {
-        return (
-          <div
-            style={{
-              backgroundColor: block,
-              height: 100,
-              width: 100,
-              float: "left",
-              margin: 15,
-            }}
-            key={block}
-          >
-            {block}
-          </div>
-        );
-      })}
 
-      <button onClick={() => addBlock("purple")}>add color!</button>
+      <div className={styles.grid}>
+        {true && <div className={styles.box1}>hello space placeholder</div>}
+        <div className={styles.box2}>
+          <div className={styles.flexCentered}>
+            <CreateFlowBlock />
+          </div>
+
+          {blocks.map((block) => {
+            return (
+              <div
+                style={{
+                  backgroundColor: block,
+                  height: 100,
+                  width: 100,
+                  float: "left",
+                  margin: 15,
+                }}
+                key={block}
+              >
+                {block}
+              </div>
+            );
+          })}
+
+          <button onClick={() => addBlock("purple")}>add color!</button>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/CreateFlowBlock/index.js
+++ client/src/components/CreateFlowBlock/index.js
@@ -1,91 +1,61 @@
 import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import cameraImg from "../../assets/camera.png";
+import styles from "./CreateFlowBlock.module.css";
 
 function CreateFlowBlock() {
-  const [image, setImage] = useState([]);
+  const [image, setImage] = useState(cameraImg);
 
-  const onDrop = (acceptedImage) => {
-    setImage(
-      acceptedImage.map((img) =>
-        Object.assign(img, {
-          preview: URL.createObjectURL(img),
-        })
-      )
-    );
+  const onDrop = (acceptedImageArray) => {
+    // No need to work with arrays here because we limit dropzone to accept only 1 file.
+    const acceptedImage = acceptedImageArray[0];
+    setImage(URL.createObjectURL(acceptedImage));
 
-    acceptedImage.forEach((img) => {
-      const reader = new FileReader();
-
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-      reader.onload = () => {
-        const binaryStr = reader.result;
-        console.log(binaryStr);
-      };
-      reader.readAsArrayBuffer(img);
-    });
+    const reader = new FileReader();
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = () => {
+      const binaryStr = reader.result;
+      console.log(binaryStr);
+    };
+    reader.readAsArrayBuffer(acceptedImage);
   };
 
-  const imgStyle = {
-    display: "block",
-    width: "auto",
-    height: "100%",
-  };
-
-  const thumb = {
-    display: "inline-flex",
-    borderRadius: 2,
-    border: "1px solid #eaeaea",
-    marginBottom: 8,
-    marginRight: 8,
-    width: 100,
-    height: 100,
-    padding: 4,
-    boxSizing: "border-box",
-  };
-
-  const thumbInner = {
-    display: "flex",
-    minWidth: 0,
-    overflow: "hidden",
-  };
-
-  const thumbnail = image.map((img) => (
-    <div style={thumb} key={img.name}>
-      <div style={thumbInner}>
-        <img src={img.preview} style={imgStyle} />
-      </div>
-    </div>
-  ));
+  const imgPreview = (
+    <img
+      src={image}
+      className={image == cameraImg ? styles.dropzoneCamera : styles.imgPreview}
+    />
+  );
 
   return (
     <div>
       <form action="/api/images" method="post" encType="multipart/form-data">
-        <div>
-          <input type="text" name="text"></input>
-        </div>
-        <div>
-          <input type="file" name="image" />
-        </div>
         <Dropzone onDrop={onDrop} maxFiles={1} accept="image/*">
           {({ getRootProps, getInputProps }) => (
-            <div
-              style={{
-                backgroundColor: "green",
-                height: 100,
-                width: 100,
-                margin: 15,
-              }}
-              {...getRootProps()}
-            >
-              <img style={{ width: "100%" }} src={cameraImg} alt="image" />
+            <div className={styles.dropzone} {...getRootProps()}>
+              {imgPreview}
               <input {...getInputProps()} />
             </div>
           )}
         </Dropzone>
+
+        <label htmlFor="title">Title</label>
+        <input
+          placeholder="title"
+          className={styles.titleText}
+          type="text"
+          name="text"
+        ></input>
+
+        <label htmlFor="description">Description</label>
+        <textarea
+          className={styles.descriptionText}
+          placeholder="description"
+          type="text"
+          name="text"
+        ></textarea>
       </form>
-      <div>{thumbnail}</div>
     </div>
   );
 }
