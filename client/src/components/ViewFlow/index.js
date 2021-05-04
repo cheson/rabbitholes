24ee417a6b10d3a72a12460924bac840a96f11a9
cheson <json@cs.stanford.edu>
2021-05-04 15:46:12 -0700

    prettified single view flow with hoverables, clickables, fonts, icons, general layout

--- client/src/components/ViewFlow/index.js
+++ client/src/components/ViewFlow/index.js
@@ -11,24 +11,54 @@ export default function ViewFlow(props) {
     props.apiService.viewFlow(flowId).then((flow) => setFlow(flow));
   }, []);
 
-  const width = 1500;
-  const height = 800;
-  const imgURL = `https://picsum.photos/${width}/${height}`;
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
+  function getRandomImageURL() {
+    const width = getRandomInt(1300, 1600);
+    const height = getRandomInt(600, 900);
+    return `https://picsum.photos/${width}/${height}`;
+  }
+
+  function onClick(url) {
+    const isTextSelected = window.getSelection().toString();
+    if (!isTextSelected) {
+      // TODO: Open other webpage in a modal
+      window.open(url);
+    }
+  }
+
+  // TODO: if no URL in flow (eg for purely notes based non-external linking blocks), then don't add clickable styling
+  // TODO: add image placeholders of a certain reasonable size so the divs don't resize as much
+  // TODO: play with vertical images and see how they are handled (ans: terribly - come up with fix)
+  // TODO: return appropriate error page for a flow id that doesn't exist
   return (
     <div>
-      <img className={styles.image} src={imgURL} />
+      <img className={styles.image} src={getRandomImageURL()} />
       <div className={styles.title}>{flow.flowTitle}</div>
+      <div className={styles.metadata}>
+        <div className={styles.author}>
+          <i className="fa fa-user-circle-o"></i> ProlificUser
+        </div>{" "}
+        <div className={styles.date}>
+          <i className="fa fa-calendar-o"></i> May 4, 2021
+        </div>
+      </div>
       <div className={styles.description}>{flow.flowDescription}</div>
       <div className={styles.list}>
         {flow.blocks?.map((block, index) => (
-          <a key={block._id} href={block.url}>
-            <li className={styles.block} key={block._id}>
-              <div className={styles.index}>{index + 1}</div>
-              <img className={styles.blockImage} src={imgURL}></img>
-              <div className={styles.description}>{block.description}</div>
-            </li>
-          </a>
+          <div
+            className={styles.block}
+            key={block._id}
+            onClick={() => onClick(block.url)}
+          >
+            <div className={styles.index}>{index + 1}</div>
+            <div className={styles.blockDescription}>{block.description}</div>
+            <img className={styles.blockImage} src={getRandomImageURL()}></img>
+          </div>
         ))}
       </div>
     </div>
