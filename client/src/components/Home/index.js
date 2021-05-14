ffee03afa657fb2001d875dcdc5086a1c271423f
cheson <json@cs.stanford.edu>
2021-05-13 23:35:47 -0700

    added skeleton for home page / landing page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h2>Home</h2>
+    <div className={styles.landingPage}>
+      <h2>curated rabbit holes</h2>
+      <div>blah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blahblah blah blah blah blah</div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/aYuxKnnn5sQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
     </div>
+    <div className={styles.otherTest}>HELLO</div>
+    </div>
+
   );
 }
