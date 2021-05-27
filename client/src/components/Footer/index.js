24e73f809e18565ff6b090b686f00660318a6371
Jason Chen <json@cs.stanford.edu>
2021-05-27 12:05:28 -0700

    Footer (#16)
    * irrelevant to footer but removed extraneous example pages
    
    * another bit of extra code removed
    
    * pinned footer to the bottom of page
    
    * added a Footer component
    
    * added autogenerated privacy policy
    
    * basic footer done

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import { PRIVACY_POLICY } from "../../constants/routes";
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.copyright}>© 2021 rabbitholes</div>
+      <div className={styles.content}>
+        flowwebsitedev@gmail.com
+        <Link to={PRIVACY_POLICY} style={{ color: "black" }}>
+          privacy policy
+        </Link>
+        🐰
+      </div>
+    </div>
+  );
+}
--- client/src/components/NavigationBar/index.js
+++ client/src/components/NavigationBar/index.js
@@ -19,7 +19,7 @@ function NavigationBar(props) {
       >
         <Navbar.Brand>
           {/* TODO: investigate bug - need to close navbar after select if moving to different pages */}
-          <Link to="/">
+          <Link to={routes.ROOT}>
             <img src={rabbitIcon} className={styles.icon}></img>
           </Link>
         </Navbar.Brand>
@@ -34,9 +34,6 @@ function NavigationBar(props) {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link eventKey="About" as={Link} to={routes.ABOUT}>
-              About
-            </Nav.Link>
             <Nav.Link eventKey="createFlow" as={Link} to={routes.CREATE_FLOW}>
               Create Flow
             </Nav.Link>
