21750bcb71cddc3c0e0c2067378eef1e3ea7397e
Jason Chen <json@cs.stanford.edu>
2021-06-18 11:13:34 -0700

    Deployment infrastructure (#17)
    * added env vars and scripts to support deployment
    
    * added example conf and nginx/docker compose conf
    
    * added tls configuration

--- server/app.js
+++ server/app.js
@@ -3,13 +3,18 @@ const usersRoutes = require("./routes/users.js");
 const flowsRoutes = require("./routes/flows.js");
 const models = require("./models");
 const admin = require("firebase-admin");
-const dotenv = require("dotenv");
 const setCurrentFirebaseUser = require("./middleware/setCurrentFirebaseUser.js");
+
+const isProduction = (process.env.NODE_ENV || "dev").toLowerCase() == "production";
+const buildDirectory = isProduction ? "./build" : "../client/build";
+if (!isProduction) {
+  const dotenv = require("dotenv");
+  dotenv.config({ path: "./dev_secrets/.env" });
+}
 
 const app = express();
 
 function initializeFirebaseAdmin(app) {
-  dotenv.config({ path: "./dev_secrets/.env" });
   const serviceAccount = require(`${process.env.GOOGLE_APPLICATION_CREDENTIALS}`);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -19,12 +24,15 @@ function initializeFirebaseAdmin(app) {
 
 models.connectDb().then(async () => {
   initializeFirebaseAdmin(app);
-  app.listen(9000, () => {
-    console.log("Example app listening on port 9000");
+  const port = process.env.PORT || 8888;
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
   });
+}).catch(function(err) {
+  console.log(err);
 });
 
-app.use(express.static("../client/build"));
+app.use(express.static(buildDirectory));
 app.use(express.json());
 app.use(setCurrentFirebaseUser);
 
@@ -32,5 +40,5 @@ app.use("/1/users", usersRoutes);
 app.use("/1/flows", flowsRoutes);
 
 app.get("*", function (req, res) {
-  res.sendFile("../client/build/index.html");
+  res.sendFile(buildDirectory + "/index.html");
 });
